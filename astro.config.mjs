c61461be6cb071d57bcb7e3f2f880b207c5e510a
edupoch <edupoch@gmail.com>
2025-05-29 19:08:39 +0200

    Probamos si hace falta siquiera el adaptador de Netlify para webs estáticas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
-
-// Desactivamos el servidor de Netlify si estamos en producción
-// https://jankraus.net/2025/02/25/a-simple-guide-to-using-astro-with-keystatic/
-const isBuild = process.env.NODE_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [vue(), ...(isBuild ? [] : [netlify()])],
+  integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
   },
