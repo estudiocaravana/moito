89ea592a881a333d99abf656fb670a17387204b0
Javi <javicocinitas@gmail.com>
2025-05-29 18:24:24 +0200

    con pruebas en deply

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  integrations: [vue(), netlify()],
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [vue()],
 });
