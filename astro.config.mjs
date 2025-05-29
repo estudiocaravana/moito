b0af70883a1656d997dcb894b806538082ae6714
edupoch <edupoch@gmail.com>
2025-05-29 18:43:30 +0200

    Desactivamos el servidor de Netlify si estamos en producción

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
 
+// Desactivamos el servidor de Netlify si estamos en producción
+// https://jankraus.net/2025/02/25/a-simple-guide-to-using-astro-with-keystatic/
+const isBuild = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [vue(), netlify()],
+  integrations: [vue(), ...(isBuild ? [] : [netlify()])],
   vite: {
     plugins: [tailwindcss()],
   },
